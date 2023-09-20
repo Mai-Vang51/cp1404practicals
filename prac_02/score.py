ca2227a5417ba307ca84a8e00e2af72533e1cac6
Mai <mai.vang@my.jcu.edu.au>
2023-09-20 22:02:13 +1000

    Add and print random score and result

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,14 +2,32 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    result = "Invalid Score"
-elif score >= 90:
-    result = "Excellent"
-elif score >= 50:
-    result = "Pass"
-else:
-    result = "Bad"
-print(f"Your score is {result}")
+
+def main():
+    score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid Score")
+
+        score = float(input("Enter score: "))
+
+    result = determine_grade(score)
+
+    print(f"Your score is {result}")
+    score = random.randint(0, 100)
+    result = determine_grade(score)
+    print(f"The score of {score} is a {result}")
+
+
+def determine_grade(score):
+    if score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Pass"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
