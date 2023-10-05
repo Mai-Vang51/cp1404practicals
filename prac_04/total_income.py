664b63cc42a72fa16132f257537e784758c2c7a2
Mai <mai.vang@my.jcu.edu.au>
2023-10-05 14:23:32 +1000

    Refactor total in print into own function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -7,18 +7,29 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
-
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    number_of_months = int(input("How many months? "))
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
-    print("\nIncome Report\n-------------")
+    total = get_total_income(incomes, number_of_months)
+
+    print_income_report(incomes, number_of_months, total)
+
+
+def get_total_income(incomes, number_of_months):
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+    return total
+
+
+def print_income_report(incomes, number_of_months, total):
+    print("\nIncome Report\n-------------")
+    for month in range(1, number_of_months + 1):
+        income = incomes[month - 1]
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}") # add space between income and total
 
 
 main()
