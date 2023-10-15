b573fc349f798a54a1439b2ab533220c9d103e60
Mai <mai.vang@my.jcu.edu.au>
2023-10-15 20:15:15 +1000

    Add name extract function

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,6 +1,24 @@
 """
 CP1404/CP5632 Practical
 Word Occurrences
-Estimate:
-Actual:
+Estimate: 1 hour
+Actual: 
 """
+
+
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = extract_name_from_email(email)
+        confirmation = input(f"Is your name {name}? Y/n ").lower()
+
+
+def extract_name_from_email(email):
+    email_component = email.split("@")
+    email_prefix = email_component[0].split(".")
+    name = " ".join(email_prefix).title()
+    return name
+
+
+main()
