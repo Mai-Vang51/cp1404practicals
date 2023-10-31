f531b0b51b45baa270503641c3b1bb367c435de0
Mai <mai.vang@my.jcu.edu.au>
2023-10-31 21:17:10 +1000

    Add docstring for init

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -5,6 +5,7 @@ class ProgrammingLanguage:
     """Represent a programming language object"""
 
     def __init__(self, field="", typing="", reflection="", year=0):
+        """Initialise programming language object"""
         self.field = field
         self.typing = typing
         self.reflection = reflection
