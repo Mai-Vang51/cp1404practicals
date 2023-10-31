cbe80570db451feaf19eb86798627d9c1f71b11a
Mai <mai.vang@my.jcu.edu.au>
2023-10-31 21:31:49 +1000

    Add docstrings for functions and constants for magic numbers

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,18 +1,30 @@
-"""CP1404/CP5632 Practical"""
+"""CP1404/CP5632 Practical - Guitar"""
+
+CURRENT_YEAR = 2022
+VINTAGE_AGE_THRESHOLD = 50
 
 
 class Guitar:
+    """Represent guitar object"""
+
     def __init__(self, name="", year=0, cost=0):
+        """Initialise guitar object
+
+        year: int, year of manufacture
+        """
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Return string of guitar object"""
         return f"{self.name} ({self.year}): {self.cost}"
 
     def get_age(self):
-        age = 2022 - self.year
+        """Calculate age of guitar by taking year of manufacture from previous year"""
+        age = CURRENT_YEAR - self.year
         return age
 
     def is_vintage(self):
-        return self.get_age() > 50
+        """Return True for guitar is vintage if object is older than 50 years"""
+        return self.get_age() > VINTAGE_AGE_THRESHOLD
