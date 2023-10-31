9ab2b686fa4f6c7106ed305cc663a709c8d9d20a
Mai <mai.vang@my.jcu.edu.au>
2023-10-31 19:09:34 +1000

    Add docstrings

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,7 +1,8 @@
-"""CP1404/CP5632 Practical"""
+"""CP1404/CP5632 Practical - Programming Language"""
 
 
 class ProgrammingLanguage:
+    """Represent a programming language object"""
     def __init__(self, field="", typing="", reflection="", year=0):
         self.field = field
         self.typing = typing
@@ -9,11 +10,14 @@ class ProgrammingLanguage:
         self.year = year
 
     def __str__(self):
+        """Return string of programming language object"""
         return f"{self.field}, {self.typing} Typing, Reflection:{self.reflection}, First appeared in " \
                f"{self.year}"
 
     def __repr__(self):
+        """Return printable representation of programming language object as string"""
         return f"{self.field}, {self.typing}, {self.reflection}, {self.year}"
 
     def is_dynamic(self):
+        """Return True or False if programming language is dynamic"""
         return self.typing == "Dynamic"
