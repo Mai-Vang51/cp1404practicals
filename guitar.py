192771206f9c906b32b3015ca1db7972a8f78854
Mai <mai.vang@my.jcu.edu.au>
2023-11-06 12:40:27 +1000

    Add code and docstrings

--- guitar.py
+++ guitar.py
@@ -1,11 +1,11 @@
 """CP1404/CP5632 Practical - Guitar"""
 
-CURRENT_YEAR = 2022
+CURRENT_YEAR = 2023
 VINTAGE_AGE_THRESHOLD = 50
 
 
 class Guitar:
-    """Represent guitar object"""
+    """Represent guitar object."""
 
     def __init__(self, name="", year=0, cost=0):
         """Initialise guitar object
@@ -17,18 +17,18 @@ class Guitar:
         self.cost = cost
 
     def __repr__(self):
-        """Return string of guitar object"""
-        return f"{self.name} ({self.year}): {self.cost}"
+        """Return string of guitar object."""
+        return f"{self.name} ({self.year}): ${self.cost}"
 
     def get_age(self):
-        """Calculate age of guitar by taking year of manufacture from previous year"""
+        """Calculate age of guitar by taking year of manufacture from previous year."""
         age = CURRENT_YEAR - self.year
         return age
 
     def is_vintage(self):
-        """Return True for guitar is vintage if object is older than 50 years"""
+        """Determine if guitar object is vintage if older than 50 years."""
         return self.get_age() > VINTAGE_AGE_THRESHOLD
 
     def __lt__(self, other):
-        """Determine the latest guitar by year to other guitar object"""
+        """Determine if the guitar object year is later than other guitar object year."""
         return self.year < other.year
--- myguitars.py
+++ myguitars.py
@@ -1,17 +1,48 @@
+"""
+CP1404/CP5632 Practical
+My Guitars
+Estimate: 1 hr 30 minutes
+Actual: 1 hr 50 minutes
+"""
+
 from guitar import Guitar
 
 
-def load_guitars_list():
-    guitars = []
-    infile = open("guitars.csv", "r")
-    for line in infile:
-        parts = line.strip().split(",")
-        guitars.append(Guitar(parts[0], parts[1], parts[2]))
+def main():
+    """Get and update guitar lists into file"""
+    guitars = load_guitars_list()
+
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        name = input("Name: ")
 
     guitars.sort()
     for guitar in guitars:
         print(guitar)
+    save_current_list(guitars)
+
+
+def load_guitars_list():
+    """Load guitar lists from file"""
+    guitars = []
+    infile = open("guitars.csv", "r")
+    for line in infile:
+        parts = line.strip().split(",")
+        guitars.append(Guitar(parts[0], int(parts[1]), parts[2]))
     infile.close()
+    return guitars
 
 
-load_guitars_list()
+def save_current_list(guitars):
+    """Save updated list to file"""
+    outfile = open("guitars.csv", "w")
+    for guitar in guitars:
+        print(f"{guitar.name}, {guitar.year}, {guitar.cost}", file=outfile)
+    outfile.close()
+
+
+main()
